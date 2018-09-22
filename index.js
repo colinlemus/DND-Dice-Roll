ef2c4d6e9d4c470b165f72dcf8b84381129a3e56
colin@thelemus.com <Colin@Colins-MacBook-Pro.local>
2018-09-21 20:13:32 -0700

    lit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/static', express.static(path.join(__dirname, 'my-app/build/static')));
+app.use('/static', express.static(path.join(__dirname, 'public/build/static')));
 
 require('./routes/api-routes.js')(app);
 require('./routes/html-routes.js')(app);
