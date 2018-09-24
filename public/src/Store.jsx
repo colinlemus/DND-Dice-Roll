9b73fbebe89587d51da9ccc2b34248cb9057357e
colin@thelemus.com <Colin@Colins-MacBook-Pro.local>
2018-09-23 22:09:31 -0700

    lit

--- public/src/Store.jsx
+++ public/src/Store.jsx
@@ -9,8 +9,9 @@ const middleware = [thunk];
 export const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(...middleware)
+    // compose(
+    //     applyMiddleware(...middleware),
+    //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // )
 );
